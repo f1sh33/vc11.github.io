050518298205e45769c5b1e86f4c31aa9bf9b92a
s1mple-Lemon <58873964+s1mple-Lemon@users.noreply.github.com>
2020-02-28 23:21:50 +0700

    Add files via upload

---
+++ init.js
@@ -0,0 +1,27 @@
+
+fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    news = data;
+})
+var x = 0;
+function them(){
+    for (var i = 0;i < 10;i++){
+        var temp;
+        var newsid = news[x++];
+        var newslink = "https://hacker-news.firebaseio.com/v0/item/" + newsid + ".json"
+        fetch(newslink)
+        .then((response) => {return response.json()})
+        .then((data) => {
+            var div = document.createElement("div");
+        var a = document.createElement("a")
+        a.innerHTML = data.title;
+        a.href = data.url;
+        div.classList.add("lists")
+        div.append(a);
+        document.getElementById("container").append(div);
+        })
+    }
+}
